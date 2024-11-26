a8158be24c32a9c3d97561ef32e6bfab4f768fb3
Vineeth V Rao <vineethvrao1@gmail.com>
2024-11-26 23:26:33 +0530

    navbar and homescreen after login

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import {LogOut, Menu, Search} from 'lucide-react';
+import { useAuthStore } from "../store/authUser";
+const Navbar = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const {user, logout} = useAuthStore();
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+  return (
+    <header className="max-w-6xl mx-auto flex flex-wrap items-center justify-between p-4 h-20">
+      <Link to={"/"}>
+        <img src="/netflix-logo.png" alt="Logo" className="w-32 sm:w-40" />
+      </Link>
+
+      {/* desktop menu */}
+      <div className="hidden sm:flex gap-5 items-center">
+        <Link to={"/"} className="hover:underline">
+          Movies
+        </Link>
+        <Link to={"/"} className="hover:underline">
+          TV Shows
+        </Link>
+        <Link to={"/"} className="hover:underline">
+          Search History
+        </Link>
+      </div>
+
+      <div className="flex gap-5 items-center z-50">
+        <Link to={"/search"} className="hover:underline">
+          <Search className="size-6 cursor-pointer"/>
+        </Link>
+        <img src={user.image} alt="avatar" className="size-6" />
+        <LogOut className="size-6 cursor-pointer" onClick={logout}/>
+        <div className="sm:hidden">
+            <Menu className="size-6 cursor-pointer" onClick={toggleMobileMenu}/>
+        </div>
+
+      </div>
+
+      {/* Mobile menu */}
+      
+        <div className={`sm:hidden absolute top-20 left-2 right-2 bg-black p-4 z-40 transition-all duration-300 ease-in-out border rounded border-gray-500 ${
+            isMobileMenuOpen ? "translate-y-0 opacity-100" : "-translate-y-10 opacity-0 pointer-events-none"
+          }`}>
+          <Link to={"/movies"} className="block hover:underline p-2" onClick={toggleMobileMenu}>
+            Movies
+          </Link>
+          <Link to={"/tvShows"} className="block hover:underline p-2" onClick={toggleMobileMenu}>
+            TV Shows
+          </Link>
+          <Link to={"/searchHistory"} className="block hover:underline p-2" onClick={toggleMobileMenu}>
+            Search History
+          </Link>
+        </div>
+      {/* <div className="w-full sm:hidden mt-4 z-50 bg-black rounder border-gray-500">
+        <Link to={"/movies"} className="block hover:underline p-2" onClick={toggleMobileMenu}>
+          Movies
+        </Link>
+        <Link to={"/tvShows"} className="block hover:underline p-2" onClick={toggleMobileMenu}>
+          TV Shows
+        </Link>
+        <Link to={"/searchHistory"} className="block hover:underline p-2" onClick={toggleMobileMenu}>
+          Search History
+        </Link>
+      </div> */}
+    </header>
+  );
+};
+
+export default Navbar;
--- frontend/src/pages/Home/HomeScreen.jsx
+++ frontend/src/pages/Home/HomeScreen.jsx
@@ -1,10 +1,11 @@
-import { useAuthStore } from "../../store/authUser"
+import Navbar from "../../components/Navbar"
 
 function HomeScreen() {
-  const {logout} = useAuthStore();
   return (
-    <div>HomeScreen
-      <button onClick={logout}>Logout</button>
+    <div className="relative h-screen text-white">
+       <Navbar/>
+       <img src="/extraction.jpg" alt="Hero img" className="absolute top-0 w-full h-full object-cover -z-50" />
+       <div className="absolute top-0 w-full h-full bg-gradient-to-b from-black/80 via-black/40 to-black/0 -z-40" aria-hidden="true"/>
     </div>
   )
 }
